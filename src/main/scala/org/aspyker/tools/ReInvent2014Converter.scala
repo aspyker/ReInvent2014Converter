b0d0157f6fcb55493dafba7258b8237c988ef6ca
Andrew Spyker <awspyker@gmail.com>
2014-11-16 00:16:07 -0800

    add youtube lookup, add in the A's

--- src/main/scala/org/aspyker/tools/ReInvent2014Converter.scala
+++ src/main/scala/org/aspyker/tools/ReInvent2014Converter.scala
@@ -8,13 +8,26 @@ import scala.xml._
 import scala.collection.JavaConverters._
 import com.github.mustachejava._
 import java.io.PrintWriter
+import com.google.gdata.client.youtube.{YouTubeQuery, YouTubeService}
+import com.google.gdata.data.youtube.{VideoEntry, VideoFeed}
+import java.net.URL;
 
 object ReInvent2014Converter {
   val log = Logger.getLogger(this.getClass)
-  val config = ConfigFactory.load();
+  val config = ConfigFactory.load()
 
   def getAmazonSlides(log:Logger, config:Config) : Array[Slideshow] = {
     return new Array[Slideshow](0)
+  }
+  
+  def getDivField(classType:String, spans:NodeSeq) : String = {
+    for (span <- spans) {
+      val clazz = (span \ "@class").text
+      if (clazz == classType) {
+        return span.text
+      }
+    }
+    return ""
   }
   
   def getSessionInfos(log:Logger, filename:String, slideShares:Array[Slideshow], config:Config, newerThanDate:DateTime) : List[SessionInfo] = {
@@ -22,48 +35,69 @@ object ReInvent2014Converter {
     
     val xml = XML.withSAXParser(new org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl().newSAXParser())
     val root = XML.load(filename)
+
+    val service = new YouTubeService(config.getString("converter.youtube.client.id"))
+    val query = new YouTubeQuery(new URL("http://gdata.youtube.com/feeds/api/videos"))
+    query.setOrderBy(YouTubeQuery.OrderBy.RELEVANCE)
+    query.setSafeSearch(YouTubeQuery.SafeSearch.NONE)
     
     val divs = root \\ "div"
     for (div <- divs) {
       val id = (div \ "@id").text
       if (id.startsWith("session_")) {
-        log.debug("processing session " + id)
-        val sessInfo = new SessionInfo();
+        val pureId = id.substring(8);
+        log.debug("processing session " + pureId)
+        val sessInfo = new SessionInfo()
         
         val spans = div \\ "span"
-        for (span <- spans) {
-          val clazz = (span \ "@class").text
-          if (clazz == "abbreviation") {
-            val sessionName = span.text.split(" +")(0);
-            sessInfo.session = sessionName;
-            log.debug("found abbreviation " + sessionName);
+        val smalls = div \\ "small"
+        val shortHumanId = getDivField("abbreviation", spans).split(" +")(0)
+        if (!shortHumanId.endsWith("-R")) {
+          sessInfo.session = shortHumanId
+          sessInfo.title = getDivField("title", spans)
+          sessInfo.abstract1 = getDivField("abstract", spans)
+          sessInfo.speakers = getDivField("speakers", smalls)
+          
+          var queryString = "AWS re:Invent 2014 | " + shortHumanId
+          var youtubeUrl = getYouTubeUrl(log, shortHumanId, queryString, query, service, newerThanDate)
+          if (youtubeUrl != null) {
+            // repeat session instead
+            queryString = "AWS re:Invent 2014 | " + shortHumanId + "R"
+            youtubeUrl = getYouTubeUrl(log, shortHumanId, queryString, query, service, newerThanDate)
           }
+      
+          log.debug("youtube url = " + youtubeUrl)
+          sessInfo.youtubeUrl = youtubeUrl
+          
+          infos ::= sessInfo
         }
-        for (span <- spans) {
-          val clazz = (span \ "@class").text
-          if (clazz == "title") {
-            sessInfo.title = span.text;
-            log.debug("found title " + span.text);
-          }
-        }
-        for (span <- spans) {
-          val clazz = (span \ "@class").text
-          if (clazz == "abstract") {
-            sessInfo.abstract1 = span.text;
-            log.debug("found abstract " + span.text);
-          }
-        }
-        for (span <- spans) {
-          val clazz = (span \ "@class").text
-          if (clazz == "speakers") {
-            sessInfo.speakers = span.text;
-            log.debug("found speakers " + span.text);
-          }
-        }
-        infos ::= sessInfo
       }
     }
     return infos
+  }
+  
+  def getYouTubeUrl(log:Logger, shortHumanId:String, queryString:String, query:YouTubeQuery, service:YouTubeService, newerThanDate:DateTime) : String = {
+    query.setFullTextQuery(queryString)
+    val videoFeed = service.query(query, classOf[VideoFeed])
+    log.debug("query to youtube = " + queryString)
+  
+    var youtubeUrl = ""
+    val vfEntriesLen = videoFeed.getEntries.size() - 1;
+    for (ii <- 0 to vfEntriesLen) {
+      val video = videoFeed.getEntries.get(0)
+      // TODO:  Ensure that common attributes of publisher, etc are correct
+      val videoTitle = video.getTitle().getPlainText()
+      if (videoTitle.contains(shortHumanId)) {
+        val videoId = video.getId().substring(video.getId().lastIndexOf(':') + 1)
+        val videoDateTime = new DateTime(video.getPublished().getValue())
+        if (videoDateTime.isAfter(newerThanDate)) {
+          youtubeUrl = "http://www.youtube.com/watch?v=" + videoId
+          log.debug("youtubeUrl = " + youtubeUrl)
+          //break // TODO: how do I break??
+        }
+      }
+    }
+    return youtubeUrl
   }
 
   def main(args:Array[String]) = {
@@ -78,7 +112,7 @@ object ReInvent2014Converter {
       log.debug("infos = " + infos)
       allInfos.infos = allInfos.infos ::: infos
     }
-    log.info("allInfos = " + allInfos)
+    log.debug("allInfos = " + allInfos)
 
     val mf = new DefaultMustacheFactory()
     val mustache = mf.compile("src/main/resources/out.mustache")
